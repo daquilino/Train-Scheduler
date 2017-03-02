665060140bef86728c5f7e15cf67f55be7225760
douglas aquilino <douglas aquilino>
2017-03-02 13:19:17 -0500

    Added index.html assets/javascript/script.js assets/css/style.css

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+var config = 
+{
+	apiKey: "AIzaSyBEXITIYqpcDc-re7KgKvhodgiEpKzviL0",
+    authDomain: "train-scheduler-a77f7.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a77f7.firebaseio.com",
+    storageBucket: "train-scheduler-a77f7.appspot.com",
+    messagingSenderId: "397554903362"
+};
+  
+ firebase.initializeApp(config);
